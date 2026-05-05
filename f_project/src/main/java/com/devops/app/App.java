df255249e2dadbd4578b7f80d7ca3fb331ff53b6
Akshaya-B-S <aksh31036@gmail.com>
2026-05-05 12:12:07 +0530

    Add files via upload

---
+++ f_project/src/main/java/com/devops/app/App.java
@@ -0,0 +1,12 @@
+package com.devops.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("DevOps Pipeline is Running Successfully!");
+    }
+
+    // A simple method we can test later
+    public String getStatus() {
+        return "Dashboard Active";
+    }
+}
